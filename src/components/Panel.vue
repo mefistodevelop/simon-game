7b7a56a469f636837188b69846f965f53181dc8b
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-08-12 11:32:22 +0300

    refactor: move control panel and simon to separate components

---
+++ src/components/Panel.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="panel">
+    <div class="info">
+      <h2 class="subtitle">Round: 1</h2>
+      <button class="button">Start</button>
+      <span class="status">You lost after 100 round</span>
+    </div>
+
+    <div class="options">
+      <h2 class="subtitle">Game options</h2>
+      <ul class="level-list">
+        <li class="level">
+          <label>
+            <input type="radio" name="level" checked>
+            easy
+          </label>
+        </li>
+        <li class="level">
+          <label>
+            <input type="radio" name="level">
+            medium
+          </label>
+        </li>
+        <li class="level">
+          <label>
+            <input type="radio" name="level">
+            hard
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+
+  .panel {
+    max-width: 25rem;
+    background-color: #fff;
+  }
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 2rem;
+    height: 15rem;
+  }
+
+  .subtitle {
+    margin: 0;
+    padding: 0;
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 10rem;
+    height: 4rem;
+
+    border: none;
+    border-radius: 1rem;
+    background-color: #77ff55;
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #fff;
+  }
+
+  .status {
+    font-size: 1.6rem;
+  }
+
+  .level-list {
+    list-style: none;
+  }
+
+  .level:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
+</style>
---
+++ src/components/Simon.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="simon">
+    <audio ref="blue-sound">
+      <source src="../assets/sounds/1.ogg" type="audio/ogg"/>
+      <source src="../assets/sounds/1.mp3" type="audio/mpeg"/>
+    </audio>
+    <audio ref="yellow-sound">
+      <source src="../assets/sounds/2.ogg" type="audio/ogg"/>
+      <source src="../assets/sounds/2.mp3" type="audio/mpeg"/>
+    </audio>
+    <audio ref="red-sound">
+      <source src="../assets/sounds/3.ogg" type="audio/ogg"/>
+      <source src="../assets/sounds/3.mp3" type="audio/mpeg"/>
+    </audio> 
+    <audio ref="green-sound">
+      <source src="../assets/sounds/4.ogg" type="audio/ogg"/>
+      <source src="../assets/sounds/4.mp3" type="audio/mpeg"/>
+    </audio>
+    
+    <div class="half-left">
+      <div class="quarter top-left" @click="playAudio('blue-sound')"></div>
+      <div class="quarter bottom-left" @click="playAudio('red-sound')"></div>
+    </div>
+
+    <div class="half-right">
+      <div class="quarter top-right" @click="playAudio('yellow-sound')"></div>
+      <div class="quarter bottom-right" @click="playAudio('yellow-sound')"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Simon',
+    methods: {
+      playAudio(ref) {
+        this.$refs[ref].play();
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  $simon-size: 13rem;
+
+  .simon {
+    display: flex;
+  }
+
+  .quarter {
+    width: $simon-size;
+    height: $simon-size;
+  }
+
+  .top {
+    &-left {
+      background-color: rgba(0, 0, 255, .5);
+      border-radius: $simon-size 0 0 0;
+
+      &:hover {
+        box-shadow: -2px -2px 1px 1px rgb(156, 156, 156);
+      }
+
+      &:active {
+        background-color: #0000ff;
+      }
+    }
+
+    &-right {
+      background-color: rgba(255, 0, 0, .5);
+      border-radius: 0 $simon-size 0 0;
+
+      &:hover {
+        box-shadow: 2px -2px 1px 1px rgb(156, 156, 156);
+      }
+
+      &:active {
+        background-color: #ff0000;
+      }
+    }
+  }
+
+  .bottom {
+    &-left {
+      background-color: rgba(251, 255, 0, 0.5);
+      border-radius: 0 0 0 $simon-size;
+
+      &:hover {
+        box-shadow: -2px 2px 1px 1px rgb(156, 156, 156);
+      }
+
+      &:active {
+        background-color: #ffff00;
+      }
+    }
+
+    &-right {
+      background-color: rgba(0, 255, 0, .5);
+      border-radius: 0 0 $simon-size 0;
+
+      &:hover {
+        box-shadow: 2px 2px 1px 1px rgb(156, 156, 156);
+      }
+
+      &:active {
+        background-color: #00ff00;
+      }
+    }
+  }
+
+</style>
